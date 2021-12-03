40ae766792963af54fb49ecfb3d1b4b4ad5623b5
Siddhesh Surve <10581786@mdbs.ie>
2021-12-03 20:21:28 +0000

    With Test Case net pay is less than equal to gross pay

--- Python_CA_One.py
+++ Python_CA_One.py
@@ -1,3 +1,5 @@
+import unittest
+
 class Employee:
 
 # Employee Object Counstructor function for employee class
@@ -74,8 +76,27 @@ def computeAllPayment(empFileName, HoursFileName):
 
 
 def main():
-    computeAllPayment("Employees.txt", "Hours.txt")
+    computeAllPayment(r"C:\Users\sidds\OneDrive\Desktop\Python_CA_One\Employees.txt", r"C:\Users\sidds\OneDrive\Desktop\Python_CA_One\Hours.txt")
 
+
+class EmployeeTest(unittest.TestCase): #creating a Employeetest class for unit test
+    
+    #Creating three functions for testing the net pay is less than equal to gross pay  
+    def testNetLessEqualGross(self):
+        e = Employee(123456, 'Green', 'Joe', 37, 16, 1.5, 70, 700) 
+        pi = e.computePayment(1,'31/10/2021')
+        self.assertLessEqual(pi['Net Pay'], pi['Gross Pay'])
+    def testNetLessEqualGrossa(self):
+        e1 = Employee(1234458, 'Shane', 'John', 39, 12, 2.6, 50, 825) 
+        pi1= e1.computePayment(40,'12/11/2021')
+        self.assertLessEqual(pi1['Net Pay'], pi1['Gross Pay'])
+    def testNetLessEqualGrossb(self):
+        e2 = Employee(923696, 'Landon', 'Steve', 39, 12, 0.5, 60, 955) 
+        pi2= e2.computePayment(68,'12/11/2021')
+        self.assertLessEqual(pi2['Net Pay'], pi2['Gross Pay'])
+
+ #Running the tests below line
 
 if __name__ == "__main__":
     main()
+    unittest.main()
